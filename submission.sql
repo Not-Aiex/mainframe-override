6e3ddf7cf3d9dfd5018d33d0ba9a19f3a4a36f23
Alex Chen <achen2291@gmail.com>
2026-07-20 22:24:40 -0700

    Successfully stopped project

--- submission.sql
+++ submission.sql
@@ -18,4 +18,39 @@ SELECT * FROM forum_accounts WHERE username LIKE 'smart-money-44';
 SELECT * FROM forum_accounts WHERE last_name LIKE 'Steele';
 
 -- Andrew, Kevin, Brad Steele
+-- Search emptystack_accounts for last name Steele
 
+SELECT * FROM emptystack_accounts WHERE last_name LIKE 'Steele';
+
+-- Andrew Steele: username = triple-cart-38, password = password456
+-- Lance Steele: username = lance-main-11, password = password789
+-- Should be Andrew Steele because Andrew actively used forum
+-- Loaded emptystack.sql and search emptystack_messages for messages from Andrew
+
+SELECT * FROM emptystack_messages WHERE emptystack_messages.from ILIKE 'triple-cart-38';
+
+-- Returned nothing, search emptystack_messages for taxi?
+
+SELECT * FROM emptystack_messages WHERE body ILIKE '%taxi%';
+
+-- id: LidWj
+-- from: your-boss-99
+-- to: triple-cart-38
+-- subject: Project TAXI
+-- body: Deploy Project TAXI by end of week. We need this out ASAP.
+-- Get credentials of your-boss-99 account
+
+SELECT * FROM emptystack_accounts WHERE username LIKE 'your-boss-99';
+
+-- username: your-boss-99
+-- password: notagaincarter
+-- first_name: Skylar
+-- last_name Singer
+-- Input into mainframe
+-- Need project ID
+
+SELECT * FROM emptystack_projects WHERE code ILIKE 'taxi';
+
+-- username: your-boss-99
+-- password: notagaincarter
+-- id: DczE0v2b
